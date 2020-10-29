883ba5256276b9728701e15c206d6990e7fd220c
AidanRosen <aidanrosen04@gmail.com>
2020-10-29 08:58:25 -0700

    Finished main menu, added submenus to graphing calculator.

--- src/centralMenu.java
+++ src/centralMenu.java
@@ -14,6 +14,7 @@ import java.awt.event.*;
 import javax.swing.border.MatteBorder;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
@@ -25,6 +26,23 @@ public class centralMenu extends JFrame { //created by Aidan <3 Rosen
         centralMenu menu = new centralMenu();
         menu.setVisible(true);
 
+    }
+
+    //action listener for A.Joseph's calculator code in the submenu of graphing calculator
+    class AjCalcAction implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            GraphingCalculatorAJ frame = new GraphingCalculatorAJ();
+            frame.setVisible(true);
+
+        }
+    }
+
+    class AaCalcAction implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            GraphMain frame = new GraphMain();
+            frame.setVisible(true);
+
+        }
     }
 
     centralMenu() {
@@ -58,52 +76,20 @@ public class centralMenu extends JFrame { //created by Aidan <3 Rosen
 
         );
         menuBar.add(vectorCalculator); //you MUST still add the actual menu item!
-        JMenu GraphingCalculatorAJ = new JMenu("Andrew Joseph's Graphing Calculator");
-        GraphingCalculatorAJ.addMenuListener(new MenuListener() {
-                                             @Override
-                                             public void menuSelected(MenuEvent e) {
-                                                 System.out.println("VectorCalculator to be opened");
-                                                 view_control.GraphingCalculatorAJ frame = new GraphingCalculatorAJ();
-                                                 frame.setVisible(true);
-                                             }
-                                             //I need to fix it to function on mouse release of the button
-                                             @Override
-                                             public void menuDeselected(MenuEvent e) {
-                                                 //May need to get rid of main method in VectorCalculator now
 
-                                             }
+        //Add Andrew Josephs menu to the menu bar. This is what contains a submenu
+        JMenu graphingCalculator = new JMenu("Andrew Joseph's Graphing Calculator");
+        menuBar.add(graphingCalculator); //you MUST still add the actual menu item!
 
-                                             @Override
-                                             public void menuCanceled(MenuEvent e) {
+        //first JMenu item under the
+        JMenuItem josephCalculator = new JMenuItem("AJ's Calculator");
+        josephCalculator.addActionListener(new AjCalcAction());
+        graphingCalculator.add(josephCalculator);
 
-                                             }
-                                         }
 
-        );
-        menuBar.add(GraphingCalculatorAJ); //you MUST still add the actual menu item!
-        JMenu mainGraphCalculator = new JMenu("A&A's Graphing Calculator");
-        mainGraphCalculator.addMenuListener(new MenuListener() {
-                                                 @Override
-                                                 public void menuSelected(MenuEvent e) {
-                                                     System.out.println("Main Graphing Calculator to be opened");
-                                                     GraphMain frame = new GraphMain();
-                                                     frame.setVisible(true);
-                                                 }
-                                                 //I need to fix it to function on mouse release of the button
-                                                 @Override
-                                                 public void menuDeselected(MenuEvent e) {
-                                                     //May need to get rid of main method in VectorCalculator now
-
-                                                 }
-
-                                                 @Override
-                                                 public void menuCanceled(MenuEvent e) {
-
-                                                 }
-                                             }
-
-        );
-        menuBar.add(mainGraphCalculator); //you MUST still add the actual menu item!
+        JMenuItem aaCalc = new JMenuItem("A&A's Graphing Calculator");
+        aaCalc.addActionListener(new AaCalcAction());
+        graphingCalculator.add(aaCalc); //you MUST still add the actual menu item!
 
     }
 }
