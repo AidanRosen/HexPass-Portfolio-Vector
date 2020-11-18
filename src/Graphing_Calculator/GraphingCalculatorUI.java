d0112ffa249a8d76e276cd17195ef39a0851bed5
Alex04 <sdalexhu@yahoo.com>
2020-11-17 22:05:06 -0800

    graphtrig, still buggy though

--- src/Graphing_Calculator/GraphingCalculatorUI.java
+++ src/Graphing_Calculator/GraphingCalculatorUI.java
@@ -1,5 +1,7 @@
 package Graphing_Calculator;
 
+import Graphing_Calculator.Control_Files.GraphMainControl;
+
 import javax.swing.*;
 import javax.swing.plaf.FontUIResource;
 import java.util.*;
@@ -7,6 +9,8 @@ import java.awt.*;
 
 //view class for displaying graph from 2d array
 public class GraphingCalculatorUI extends JFrame {
+
+    public static Font f = new Font("Arial", Font.PLAIN, 5);
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -21,18 +25,26 @@ public class GraphingCalculatorUI extends JFrame {
 
     public GraphingCalculatorUI() { //constructor
         //print out the 2d array as a new window
-        Font f = new Font("Arial", Font.PLAIN, 5);
         getContentPane().setBackground(new Color(175, 238, 238));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setBounds(100, 100, 1920, 1080); //sets size of the window
         getContentPane().setLayout(null);
-       // repaint();
 
-        for (int i = 0; i < 101; i++) {
-            for (int j = 0; j < 101; j++) {
-                GraphMain.Graph[i][j].setBounds(0 + 10 * j, 0 + 10 * i, 10, 10);
-                GraphMain.Graph[i][j].setFont(f);
-                getContentPane().add(GraphMain.Graph[i][j]); //prints out graph
+        if (GraphMainControl.choice.equals("3")) { //trig graphs require a different display
+            for (int i = 0; i < 97; i++) {
+                for (int j = 0; j < 97; j++) {
+                    GraphMain.Graph[i][j].setBounds(0 + 10 * j, 0 + 10 * i, 10, 10);
+                    GraphMain.Graph[i][j].setFont(f);
+                    getContentPane().add(GraphMain.Graph[i][j]); //prints out graph
+                }
+            }
+        } else {
+            for (int i = 0; i < 101; i++) {
+                for (int j = 0; j < 101; j++) {
+                    GraphMain.Graph[i][j].setBounds(0 + 10 * j, 0 + 10 * i, 10, 10);
+                    GraphMain.Graph[i][j].setFont(f);
+                    getContentPane().add(GraphMain.Graph[i][j]); //prints out graph
+                }
             }
         }
     }
--- src/Graphing_Calculator/Model_Files/GraphTrig.java
+++ src/Graphing_Calculator/Model_Files/GraphTrig.java
@@ -6,12 +6,52 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.lang.Math.*; //for trig functions
+import java.util.ArrayList;
+//todo: x axes scales incorrectly
+//todo: y axis should only display every 3rd number
+//todo: only sin works, all others show as y=0
+//todo: x-ints not showing
 
+import Graphing_Calculator.Control_Files.GraphPolyControl;
 import Graphing_Calculator.Control_Files.GraphTrigControl;
 import Graphing_Calculator.GraphMain; //allows for calling of GraphMain. methods, which is outside the package Model_Files and therefore cannot be called without being imported
 import Graphing_Calculator.GraphingCalculatorUI; //allows for calling of GraphingCalculatorUI. methods
 
 public class GraphTrig extends JFrame {
+
+
+    public static ArrayList<String> inputsValsCoef = new ArrayList<String>();
+    public static ArrayList<String> inputsValsFunc = new ArrayList<String>();
+    public static ArrayList<String> inputsValsExp = new ArrayList<String>();
+
+    public static JButton go = new JButton("Graph");
+    public static JButton next = new JButton("Enter next term");
+    public static JButton sinButton = new JButton("sin");
+    public static JButton cosButton = new JButton("cos");
+    public static JButton tanButton = new JButton("tan");
+    public static JButton cscButton = new JButton("csc");
+    public static JButton secButton = new JButton("sec");
+    public static JButton cotButton = new JButton("cot");
+
+    public static  JLabel label = new JLabel("θ");
+    public static JLabel funcLabel = new JLabel("Func");
+    public static JLabel promptX = new JLabel("Please enter the x magnitude/zoom level (0.1-10):");
+    public static JLabel promptY = new JLabel("Please enter the y magnitude/zoom level (0.1-10):");
+    public static JLabel errorMessage = new JLabel("");
+    public static JLabel XIntColon = new JLabel("Approximate x-intercept(s): ");
+    public static JLabel YIntColon = new JLabel("Y-intercept: ");
+
+    public static JTextField inputBoxCoef = new JTextField("");
+    public static JTextField inputBoxExp = new JTextField("");
+    public static JTextField inputBoxX = new JTextField(10);
+    public static JTextField inputBoxY = new JTextField(10);
+
+    public static ArrayList<Double> XInts = new ArrayList<Double>();
+    public static ArrayList<JLabel> XIntDisplays = new ArrayList<JLabel>();
+    public static double YInt = 0;
+    public static JLabel YIntDisplay = new JLabel();
+
+    public static String magX = null, magY = null;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -26,6 +66,237 @@ public class GraphTrig extends JFrame {
 
 
     public GraphTrig() { //constructor for initializing labels and textboxes and add them to getcontentpane
+        getContentPane().setBackground(new Color(175, 238, 238));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 1920, 1080); //sets size of the window
+        getContentPane().setLayout(null);
 
+        label.setBounds(225, 100, 40, 50);
+        go.setBounds(400, 450, 200, 100);
+        next.setBounds(100, 450, 200, 100);
+        sinButton.setBounds(50, 350, 70, 50);
+        cosButton.setBounds(125, 350, 70, 50);
+        tanButton.setBounds(200, 350, 70, 50);
+        cscButton.setBounds(275, 350, 70, 50);
+        secButton.setBounds(350, 350, 70, 50);
+        cotButton.setBounds(425, 350, 70, 50);
+
+        funcLabel.setBounds(100, 100, 40, 50);
+        promptX.setBounds(50, 200, 400, 40);
+        promptY.setBounds(450, 200, 400, 40);
+        errorMessage.setBounds(460, 500, 1000, 50);
+
+        inputBoxCoef.setBounds(25, 100, 70, 50);
+        inputBoxExp.setBounds(145,75, 70, 40);
+        inputBoxX.setBounds(50, 250, 400, 40);
+        inputBoxY.setBounds(450, 250, 400, 40);
+
+        getContentPane().add(label);
+        getContentPane().add(go);
+        getContentPane().add(next);
+        getContentPane().add(sinButton);
+        getContentPane().add(cosButton);
+        getContentPane().add(tanButton);
+        getContentPane().add(cscButton);
+        getContentPane().add(secButton);
+        getContentPane().add(cotButton);
+
+        getContentPane().add(funcLabel);
+        getContentPane().add(promptX);
+        getContentPane().add(promptY);
+        getContentPane().add(errorMessage);
+
+        getContentPane().add(inputBoxCoef);
+        getContentPane().add(inputBoxExp);
+        getContentPane().add(inputBoxX);
+        getContentPane().add(inputBoxY);
+
+        go.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (inputBoxCoef.getText().equals("")) { //if user left textbox blank
+                    inputBoxCoef.setText("0");
+                }
+
+                if (inputBoxExp.getText().equals("")) {
+                    inputBoxExp.setText("0");
+                }
+
+                if (funcLabel.getText().equals("Func")) {
+                    funcLabel.setText("sin");
+                }
+
+                inputsValsCoef.add(inputBoxCoef.getText()); //saves the text from the input box into an arraylist when user chooses to graph
+                inputsValsFunc.add(funcLabel.getText());
+                inputsValsExp.add(inputBoxExp.getText());
+
+                magX = inputBoxX.getText();
+                magY = inputBoxY.getText();
+
+                if (magX != null && magY != null && inputsValsCoef.size() == inputsValsExp.size() && inputsValsExp.size() == inputsValsFunc.size()) { //is user has inputted these values
+                    Graph(inputsValsCoef, inputsValsFunc, inputsValsExp, magX, magY);
+                }
+            }
+        });
+
+        GraphTrigControl.main(null);
+    }
+
+    public void Graph(ArrayList<String> coefficients1, ArrayList<String> functions1, ArrayList<String> exponents1, String magX1, String magY1) {
+        getContentPane().remove(errorMessage);
+
+        for (int i = 0; i < coefficients1.size(); i++) {
+            try {
+                double numTest = Double.parseDouble(coefficients1.get(i)); //tests the see if the value in the arraylist at index/location i is a number
+            } catch (NumberFormatException nfe) {
+                errorMessage.setText("Inputs must be numbers");
+                this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING)); //closes current window
+                main(null); //creates new window and reruns program
+            }
+
+            try {
+                double numTest = Double.parseDouble(exponents1.get(i));
+            } catch (NumberFormatException nfe) {
+                errorMessage.setText("Inputs must be numbers");
+                this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+                main(null);
+            }
+        }
+
+        String[] inputs = {magX1, magY1}; //checks that all three of these are numbers
+
+        for (int i = 0; i < 2; i++) {
+            try {
+                double numTest = Double.parseDouble(inputs[i]);
+            } catch (NumberFormatException nfe) {
+                errorMessage.setText("Inputs must be numbers");
+                this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+                main(null);
+            }
+        }
+
+        double magX = Double.parseDouble(magX1), magY = Double.parseDouble(magY1); //converts input into numbers
+
+        if (magX < 0.1 || magX > 10 || magY < 0.1 || magY > 10) {
+            errorMessage.setText("Magnitudes must be between 0.1 and 10");
+            this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+            main(null);
+        }
+
+        double domainLower = -8.0 * magX * Math.PI;
+        double domainUpper = 8.0 * magX * Math.PI;
+        double rangeLower = -16.0 * magY;
+        double rangeUpper = 16.0 * magY;
+        double y = 0;
+
+        for (int i = 0; i < 97; i++) { //requires a different set up for axes labeled with grid value in radians
+            for (int j = 0; j < 97; j++) {
+                if (i == 48) {
+                    if (j > 48) { //for sorting out negatives and positives
+                        if (j % 3 == 0) { //only label every 3 grid boxes
+                            GraphMain.Graph[i][j] = new JLabel(Double.toString((j * magX) / Math.PI - (domainUpper / Math.PI)));
+                        } else {
+                            GraphMain.Graph[i][j] = new JLabel("-");
+                        }
+
+                        if (Math.floor(((j * magX) / Math.PI - (domainUpper / Math.PI))) == (j * magX) / Math.PI - (domainUpper / Math.PI)) { //if the value is something .0 (so the same as an integer), only display the int and not .0 to save space
+                            GraphMain.Graph[i][j].setText(Integer.toString((int)((j * magX) / Math.PI - (domainUpper / Math.PI)))); //converts double to int, them to string, then to jlabel
+                        }
+                    } else if (j < 48) { //if negative
+                        if (j % 3 == 0) {
+                            GraphMain.Graph[i][j] = new JLabel("-" + Double.toString((j * magX) / Math.PI - (domainUpper / Math.PI)));
+                        } else {
+                            GraphMain.Graph[i][j] = new JLabel("-");
+                        }
+
+                        if (Math.floor(((j * magX) / Math.PI - (domainUpper / Math.PI))) == (j * magX) / Math.PI - (domainUpper / Math.PI)) {
+                            GraphMain.Graph[i][j].setText("-" + Integer.toString((int)((j * magX) / Math.PI - (domainUpper / Math.PI))));
+                        }
+                    } else if (j == 48) {
+                        GraphMain.Graph[i][j] = new JLabel("0");
+                    }
+                } else if (j == 48) {
+                    if (i < 48) {
+                        if (i % 3 == 0) { //only label every 3 grid boxes, or every pi/2 radians
+                            GraphMain.Graph[i][j] = new JLabel(Double.toString((rangeUpper - i * magY / 3)));
+                        } else {
+                            GraphMain.Graph[i][j] = new JLabel("|");
+                        }
+
+                        if (Math.floor(rangeUpper - i * magY) == rangeUpper - i * magY) {
+                            GraphMain.Graph[i][j].setText(Integer.toString((int)(rangeUpper - i * magY / 3)));
+                        }
+                    } else {
+                        if (i % 3 == 0) {
+                            GraphMain.Graph[i][j] = new JLabel("-" + Double.toString(i * magY / 3 - rangeUpper));
+                        } else {
+                            GraphMain.Graph[i][j] = new JLabel("|");
+                        }
+
+                        if (Math.floor(i * magY - rangeUpper) == i * magY - rangeUpper) {
+                            GraphMain.Graph[i][j].setText("-" + Integer.toString((int)(i * magY / 3 - rangeUpper)));
+                        }
+                    }
+                } else {
+                    GraphMain.Graph[i][j] = new JLabel(" ");
+                }
+            }
+        }
+
+        for (double x = domainLower; x < domainUpper; x += magX * (Math.PI / 6)) { //Graphs into a 2d array, x axis advances by pi/6 radians at a time
+            y = 0.0; //resets y for every new x-value
+            for (int i = 0; i < coefficients1.size(); i++) {
+                if (functions1.get(i).equals("sin")) { //calculations
+                    y += (Math.pow(Math.sin(x), Double.parseDouble(exponents1.get(i))) * Double.parseDouble(coefficients1.get(i)));
+                } else if (functions1.get(i).equals("cos")) {
+                    y += (Math.pow(Math.cos(x), Double.parseDouble(exponents1.get(i))) * Double.parseDouble(coefficients1.get(i)));
+                } else if (functions1.get(i).equals("tan")) {
+                    y += (Math.pow(Math.tan(x), Double.parseDouble(exponents1.get(i))) * Double.parseDouble(coefficients1.get(i)));
+                } else if (functions1.get(i).equals("csc")) {
+                    y += (Math.pow((1 / Math.sin(x)), Double.parseDouble(exponents1.get(i))) * Double.parseDouble(coefficients1.get(i)));
+                } else if (functions1.get(i).equals("sec")) {
+                    y += (Math.pow((1 / Math.cos(x)), Double.parseDouble(exponents1.get(i))) * Double.parseDouble(coefficients1.get(i)));
+                } else if (functions1.get(i).equals("cot")) {
+                    y += (Math.pow((1 / Math.tan(x)), Double.parseDouble(exponents1.get(i))) * Double.parseDouble(coefficients1.get(i)));
+                }
+            }
+
+            if (Math.abs(y) < rangeUpper && Math.abs(y) > rangeLower) { //puts x,y coordinates into 2d array if they are within -50,50 or otherwise specified by the magnitude
+                GraphMain.Graph[48 - (int) Math.round(((y * 3) / magY))][48 + (int) Math.round(((x / (Math.PI / 6)) / magX))].setText("⬤"); //inputs the coordinate into the 2darray
+
+                if (y == 0.0) { //approx x-intercept
+                    XInts.add(x); //stores current x-value as an x-int
+                }
+
+                if (x == 0.0) { //y-int
+                    YInt = y;
+                }
+            }
+        }
+
+        GraphingCalculatorUI.main(null); //prints out graph
+
+        XIntColon.setBounds(25, 700, 200, 50);
+        getContentPane().add(XIntColon);
+        YIntColon.setBounds(1000, 50, 100, 50);
+        getContentPane().add(YIntColon);
+        int wrapText = 0;
+
+        for (int i = 0; i < XInts.size(); i++) {
+            XIntDisplays.add(new JLabel(Double.toString(XInts.get(i)) + ", ")); //gets the value at xints arraylist[i], converts it into a string, and puts that string on a new jlabel
+            int rowNumber = (int)Math.ceil((i + 1) / 12.0); //for wrapping around the edge of the jframe to a new row; can fit ~12 x-ints on 1 row
+            wrapText++;
+
+            if (i % 12 == 0) { //if a new row is starting, shift the labels back to the left side of the screen
+                wrapText = 0;
+            }
+
+            XIntDisplays.get(i).setBounds(225 + wrapText  * 100, 640 + rowNumber * 60, 100, 50);
+            getContentPane().add(XIntDisplays.get(i)); //adds that new jlabel to the screen
+        }
+
+        YIntDisplay.setText(Double.toString(YInt)); //for y-int display
+        YIntDisplay.setBounds(1100, 50, 200, 50);
+        getContentPane().add(YIntDisplay);
     }
 }
