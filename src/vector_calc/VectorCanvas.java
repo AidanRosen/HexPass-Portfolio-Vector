86f7b88f514b693559aeeecb13ae7402fab164a9
AidanRosen <43625295+AidanRosen@users.noreply.github.com>
2020-11-22 13:38:41 -0800

    Update VectorCanvas.java

--- src/vector_calc/VectorCanvas.java
+++ src/vector_calc/VectorCanvas.java
@@ -10,8 +10,8 @@ public class VectorCanvas extends JPanel{
     //Works by creating a new frame/window and THEN drawing the vector
 
     private Vector<VectorDraw> legs = new Vector<>();
-    private Vector<HeadDraw> heads = new Vector<>();
-    private ResultantDraw solid;
+    private Vector<HeadDraw> heads = new Vector<>(); //Vector is an extension of stack and acts similarly to an ArrayList
+    private ResultantDraw solid; //Need to use a separate object that is an instanced variable so that it can render in the for loop below 
 
     public VectorCanvas (double magnitude, double direction, double horizontal, double vertical){
         JFrame jF = new JFrame();
@@ -50,9 +50,11 @@ public class VectorCanvas extends JPanel{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        //MUST render everything at once and we MUST do that with a for loop so that we avoid repaint painting over and erasing previous components
         for (VectorDraw shape : legs) {
             shape.drawRect(g);
         }
+        //Here render the Resultant defined at the very top (ResultantDraw solid) outside a for loop because there is only one 
         solid.drawResult(g);
         for (HeadDraw tri : heads) {
             tri.drawTri(g);
